1dae2219eabeb07ebd350b5494daa1b177903396
CHANN <chann@chann.kr>
2025-07-17 18:28:25 +0900

    update: refine codes and fix minor bugs

--- gss.user.js
+++ gss.user.js
@@ -3,200 +3,396 @@
 // @description  Navigate google search with custom shortcuts
 // @namespace    https://github.com/channprj/google-search-navigator
 // @icon         https://user-images.githubusercontent.com/1831308/60544915-c043e700-9d54-11e9-9eb0-5c80c85d3a28.png
-// @version      0.6
+// @version      0.7
 // @author       channprj
 // @run-at       document-end
 // @include      http*://*.google.tld/search*
 // @include      http*://*.google.*/search*
 // ==/UserScript==
 
-const styles = {
-  selectedLink:
-    "padding-left:8px; margin-left:-12px; border-left:4px solid red;",
-  normal: "",
-};
+(function () {
+  "use strict";
 
-// Selectors
-let resultElements = document.querySelectorAll(".MjjYud");
-const nestedResultElements = document.querySelectorAll(".A6K0A");
-if (nestedResultElements.length > 0) {
-  resultElements = nestedResultElements;
-}
-
-const searchInputElement = document.querySelector("div textarea");
-const contentWrapper = document.querySelector("#rcnt");
-
-const isTextElementFocused = () => {
-  const el = document.activeElement;
-  return (
-    el &&
-    ((el.tagName.toLowerCase() === "input" && el.type === "text") ||
-      el.tagName.toLowerCase() === "textarea")
-  );
-};
-
-const moveCursorToEnd = (element) => {
-  element.focus();
-  if (element.setSelectionRange) {
-    const len = element.value.length * 2;
-    element.setSelectionRange(len, len);
-  } else {
-    element.value = element.value;
-  }
-};
-
-const isValidResultNode = (node) => {
-  return node && node.childElementCount > 0 && node.offsetHeight > 0;
-};
-
-const findNextValidIndex = (currentIndex, childNodes) => {
-  let nextIndex = currentIndex + 1;
-  if (
-    nextIndex < childNodes.length &&
-    !isValidResultNode(childNodes[nextIndex])
-  ) {
-    nextIndex++;
-  }
-  return nextIndex < childNodes.length ? nextIndex : currentIndex;
-};
-
-const findPrevValidIndex = (currentIndex, childNodes) => {
-  let prevIndex = currentIndex - 1;
-  if (prevIndex >= 0 && !isValidResultNode(childNodes[prevIndex])) {
-    prevIndex--;
-  }
-  return prevIndex >= 0 ? prevIndex : currentIndex;
-};
-
-const setHighlight = (index, childNodes) => {
-  if (
-    index >= 0 &&
-    index < childNodes.length &&
-    isValidResultNode(childNodes[index])
-  ) {
-    childNodes[index].style.cssText = styles.selectedLink;
-    childNodes[index].scrollIntoView({
+  // Configuration
+  const CONFIG = {
+    styles: {
+      selectedLink:
+        "padding-left:8px; margin-left:-12px; border-left:4px solid red;",
+      normal: "",
+    },
+    selectors: {
+      resultElements: ".MjjYud",
+      nestedResultElements: ".A6K0A",
+      searchInput: "div textarea",
+      contentWrapper: "#rcnt",
+      nextButton: "#pnnext",
+      prevButton: "#pnprev",
+    },
+    scrollBehavior: {
       behavior: "smooth",
       block: "center",
-    });
-  }
-};
+    },
+  };
 
-const clearHighlight = (index, childNodes) => {
-  if (index >= 0 && index < childNodes.length) {
-    childNodes[index].style.cssText = styles.normal;
-  }
-};
-
-const clickItem = (index, childNodes) => {
-  if (
-    index >= 0 &&
-    index < childNodes.length &&
-    isValidResultNode(childNodes[index])
-  ) {
-    const selectedLink = childNodes[index].getElementsByTagName("a")[0];
-    if (selectedLink) {
-      selectedLink.click();
+  // DOM Elements Cache
+  class DOMCache {
+    constructor() {
+      this.refresh();
     }
-  }
-};
 
-const initializeFirstResult = (resultElements) => {
-  if (resultElements && resultElements.length > 0) {
-    let firstValidIndex = 0;
-    while (
-      firstValidIndex < resultElements.length &&
-      !isValidResultNode(resultElements[firstValidIndex])
-    ) {
-      firstValidIndex++;
-    }
-    if (firstValidIndex < resultElements.length) {
-      const firstLink =
-        resultElements[firstValidIndex].getElementsByTagName("a")[0];
-      if (firstLink) {
-        firstLink.focus();
+    refresh() {
+      this._resultElements = document.querySelectorAll(
+        CONFIG.selectors.resultElements
+      );
+      const nestedElements = document.querySelectorAll(
+        CONFIG.selectors.nestedResultElements
+      );
+
+      // Use nested elements if available (for specialized search results)
+      if (nestedElements.length > 0) {
+        this._resultElements = nestedElements;
       }
-      setHighlight(firstValidIndex, resultElements);
-      return firstValidIndex;
+
+      this._searchInput = document.querySelector(CONFIG.selectors.searchInput);
+      this._contentWrapper = document.querySelector(
+        CONFIG.selectors.contentWrapper
+      );
+    }
+
+    get resultElements() {
+      return this._resultElements;
+    }
+
+    get searchInput() {
+      return this._searchInput;
+    }
+
+    get contentWrapper() {
+      return this._contentWrapper;
     }
   }
-  return 0;
-};
 
-const handlePagination = (direction) => {
-  if (
-    document.activeElement.tagName === "INPUT" ||
-    document.activeElement.tagName === "TEXTAREA" ||
-    document.activeElement.contentEditable === "true"
-  ) {
-    return;
+  const domCache = new DOMCache();
+
+  // Utility Functions
+  const Utils = {
+    isTextElementFocused() {
+      const el = document.activeElement;
+      return (
+        el &&
+        ((el.tagName.toLowerCase() === "input" &&
+          (el.type === "text" ||
+            el.type === "search" ||
+            el.type === "email" ||
+            el.type === "password" ||
+            el.type === "url")) ||
+          el.tagName.toLowerCase() === "textarea" ||
+          el.contentEditable === "true")
+      );
+    },
+
+    moveCursorToEnd(element) {
+      if (!element) return;
+
+      element.focus();
+      if (element.setSelectionRange) {
+        const len = element.value.length * 2;
+        element.setSelectionRange(len, len);
+      } else {
+        element.value = element.value;
+      }
+    },
+
+    isValidResultNode(node) {
+      return node && node.childElementCount > 0 && node.offsetHeight > 0;
+    },
+
+    debounce(func, wait) {
+      let timeout;
+      return function executedFunction(...args) {
+        const later = () => {
+          clearTimeout(timeout);
+          func(...args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+      };
+    },
+  };
+
+  // Navigation Controller
+  class NavigationController {
+    constructor() {
+      this.focusIndex = 0;
+      this.initialize();
+    }
+
+    initialize() {
+      this.focusIndex = this.initializeFirstResult();
+    }
+
+    findNextValidIndex(currentIndex) {
+      const elements = domCache.resultElements;
+      let nextIndex = currentIndex + 1;
+
+      while (
+        nextIndex < elements.length &&
+        !Utils.isValidResultNode(elements[nextIndex])
+      ) {
+        nextIndex++;
+      }
+
+      return nextIndex < elements.length ? nextIndex : currentIndex;
+    }
+
+    findPrevValidIndex(currentIndex) {
+      const elements = domCache.resultElements;
+      let prevIndex = currentIndex - 1;
+
+      while (prevIndex >= 0 && !Utils.isValidResultNode(elements[prevIndex])) {
+        prevIndex--;
+      }
+
+      return prevIndex >= 0 ? prevIndex : currentIndex;
+    }
+
+    setHighlight(index) {
+      const elements = domCache.resultElements;
+      if (
+        index >= 0 &&
+        index < elements.length &&
+        Utils.isValidResultNode(elements[index])
+      ) {
+        elements[index].style.cssText = CONFIG.styles.selectedLink;
+        elements[index].scrollIntoView(CONFIG.scrollBehavior);
+      }
+    }
+
+    clearHighlight(index) {
+      const elements = domCache.resultElements;
+      if (index >= 0 && index < elements.length) {
+        elements[index].style.cssText = CONFIG.styles.normal;
+      }
+    }
+
+    clickItem(index) {
+      const elements = domCache.resultElements;
+      if (
+        index >= 0 &&
+        index < elements.length &&
+        Utils.isValidResultNode(elements[index])
+      ) {
+        const selectedLink = elements[index].getElementsByTagName("a")[0];
+        if (selectedLink) {
+          selectedLink.click();
+        }
+      }
+    }
+
+    initializeFirstResult() {
+      const elements = domCache.resultElements;
+      if (!elements || elements.length === 0) return 0;
+
+      let firstValidIndex = 0;
+      while (
+        firstValidIndex < elements.length &&
+        !Utils.isValidResultNode(elements[firstValidIndex])
+      ) {
+        firstValidIndex++;
+      }
+
+      if (firstValidIndex < elements.length) {
+        this.setHighlight(firstValidIndex);
+        return firstValidIndex;
+      }
+
+      return 0;
+    }
+
+    navigateNext() {
+      this.clearHighlight(this.focusIndex);
+      this.focusIndex = this.findNextValidIndex(this.focusIndex);
+      this.setHighlight(this.focusIndex);
+    }
+
+    navigatePrev() {
+      this.clearHighlight(this.focusIndex);
+      this.focusIndex = this.findPrevValidIndex(this.focusIndex);
+      this.setHighlight(this.focusIndex);
+    }
+
+    activateCurrentResult() {
+      this.clickItem(this.focusIndex);
+    }
+
+    refresh() {
+      domCache.refresh();
+      this.initialize();
+    }
   }
 
-  const pageButton = document.getElementById(
-    direction === "next" ? "pnnext" : "pnprev"
-  );
-  pageButton?.click();
-};
+  // Pagination Handler
+  const PaginationHandler = {
+    handlePagination(direction) {
+      if (Utils.isTextElementFocused()) {
+        return;
+      }
 
-const focusSearchInput = (searchInput) => {
-  moveCursorToEnd(searchInput);
-};
+      const buttonId =
+        direction === "next"
+          ? CONFIG.selectors.nextButton
+          : CONFIG.selectors.prevButton;
+      const pageButton = document.querySelector(buttonId);
 
-// WIP
-const observer = new MutationObserver((mutations) => {});
+      if (pageButton) {
+        pageButton.click();
+      }
+    },
+  };
 
-observer.observe(document.body, {
-  childList: true,
-  subtree: true,
-});
+  // Search Input Handler
+  const SearchInputHandler = {
+    focusSearchInput() {
+      const searchInput = domCache.searchInput;
+      if (searchInput) {
+        Utils.moveCursorToEnd(searchInput);
+      }
+    },
 
-// Initialize with first valid result
-let focusIndex = initializeFirstResult(resultElements);
+    blurSearchInput() {
+      const searchInput = domCache.searchInput;
+      if (searchInput) {
+        searchInput.blur();
+      }
+    },
+  };
 
-// TODO: Support nested rich lists (Image, Video, News, etc.)
+  // Initialize navigation controller
+  const navigation = new NavigationController();
 
-// TODO: Support paginator navigation
+  // Keyboard Event Handler
+  const KeyboardHandler = {
+    handleKeyEvent(event) {
+      const keyCode = event.code;
 
-window.addEventListener("keyup", (event) => {
-  event.preventDefault();
-  const keyCode = event.code;
+      // Navigation keys
+      if (keyCode === "KeyJ" || keyCode === "ArrowDown") {
+        event.preventDefault();
+        navigation.navigateNext();
+        return;
+      }
 
-  if (keyCode === "KeyJ" || keyCode === "ArrowDown") {
-    clearHighlight(focusIndex, resultElements);
-    focusIndex = findNextValidIndex(focusIndex, resultElements);
-    setHighlight(focusIndex, resultElements);
-    return;
+      if (keyCode === "KeyK" || keyCode === "ArrowUp") {
+        event.preventDefault();
+        navigation.navigatePrev();
+        return;
+      }
+
+      // Activation key
+      if (keyCode === "Enter" && !Utils.isTextElementFocused()) {
+        event.preventDefault();
+        navigation.activateCurrentResult();
+        return;
+      }
+
+      // Pagination keys
+      if (keyCode === "KeyL" || keyCode === "ArrowRight") {
+        event.preventDefault();
+        PaginationHandler.handlePagination("next");
+        return;
+      }
+
+      if (keyCode === "KeyH" || keyCode === "ArrowLeft") {
+        event.preventDefault();
+        PaginationHandler.handlePagination("prev");
+        return;
+      }
+
+      // Escape key
+      if (keyCode === "Escape") {
+        event.preventDefault();
+        const contentWrapper = domCache.contentWrapper;
+        if (contentWrapper) {
+          contentWrapper.click();
+        }
+        SearchInputHandler.blurSearchInput();
+        navigation.setHighlight(navigation.focusIndex);
+        return;
+      }
+
+      // Search focus key
+      if (keyCode === "Slash") {
+        event.preventDefault();
+        SearchInputHandler.focusSearchInput();
+        return;
+      }
+    },
+  };
+
+  // DOM Observer for dynamic content
+  const DOMObserver = {
+    observer: null,
+
+    init() {
+      this.observer = new MutationObserver(
+        Utils.debounce((mutations) => {
+          // Check if search results have changed
+          const hasNewResults = mutations.some((mutation) =>
+            Array.from(mutation.addedNodes).some(
+              (node) =>
+                node.nodeType === Node.ELEMENT_NODE &&
+                ((node.matches &&
+                  node.matches(CONFIG.selectors.resultElements)) ||
+                  (node.querySelector &&
+                    node.querySelector(CONFIG.selectors.resultElements)))
+            )
+          );
+
+          if (hasNewResults) {
+            navigation.refresh();
+          }
+        }, 250)
+      );
+
+      this.observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+      });
+    },
+
+    disconnect() {
+      if (this.observer) {
+        this.observer.disconnect();
+      }
+    },
+  };
+
+  // Event Listeners
+  const setupEventListeners = () => {
+    window.addEventListener("keyup", KeyboardHandler.handleKeyEvent);
+
+    // Handle page unload
+    window.addEventListener("beforeunload", () => {
+      DOMObserver.disconnect();
+    });
+  };
+
+  // Initialize the application
+  const init = () => {
+    try {
+      DOMObserver.init();
+      setupEventListeners();
+
+      console.log("Google Search Navigator initialized successfully");
+    } catch (error) {
+      console.error("Failed to initialize Google Search Navigator:", error);
+    }
+  };
+
+  // Start the application
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", init);
+  } else {
+    init();
   }
-
-  if (keyCode === "KeyK" || keyCode === "ArrowUp") {
-    clearHighlight(focusIndex, resultElements);
-    focusIndex = findPrevValidIndex(focusIndex, resultElements);
-    setHighlight(focusIndex, resultElements);
-    return;
-  }
-
-  if (keyCode === "Enter" && !isTextElementFocused()) {
-    clickItem(focusIndex, resultElements);
-    return;
-  }
-
-  if (keyCode === "KeyL" || keyCode === "ArrowRight") {
-    handlePagination("next");
-    return;
-  }
-
-  if (keyCode === "KeyH" || keyCode === "ArrowLeft") {
-    handlePagination("prev");
-    return;
-  }
-
-  if (keyCode === "Escape") {
-    contentWrapper.click();
-    searchInputElement.blur();
-    setHighlight(focusIndex, resultElements);
-    return;
-  }
-});
-
-// const pagination = document.getElementById('navcnt').getElementsByTagName('tr')
-// pagination[0].children
+})();
